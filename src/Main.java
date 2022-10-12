da106f7f8eb209421cf019ada9ad75185609b88a
Mehanik <Ivanforsag@list.ru>
2022-10-12 23:42:16 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,51 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static byte[][] matr;
-    public static List<byte[][]> list = new ArrayList<>();
+    public static byte[][] matrix;                                        // основное поле
+    public static boolean[][] array;                                      // вспомогательный массив для нахождения прямоугольников
+    public static List<byte[][]> list = new ArrayList<>();                // список прямоугольников на поле
 
-    public static int exist1(int y, int x, byte[][] matrix) {
-        if (outBounds(y, x, matrix)) {
-            return -1;
+    public static boolean existNotFound(int y, int x) {                   // соприкосновения плодородных участков до нахождения вершины
+        if (outBounds(y, x)) {
+            return false;
         }
         int i = 0;
         for (int setY = -1; setY <= 1; setY++) {
             for (int setX = -1; setX <= 1; setX++) {
-                if (outBounds(setY + y, setX + x, matrix)) continue;
+                if (outBounds(setY + y, setX + x)) continue;
                 i += matrix[setY + y][setX + x];
             }
         }
-        return i;
+        return i > 1;
     }
 
-    public static boolean outBounds(int y, int x, byte[][] matrix) {
+    public static void existFound(Node node, List<Node> list) {            // поиск остальных плодородных участков после нахождения первого
+
+        for (int setY = -1; setY <= 1; setY++) {
+            for (int setX = -1; setX <= 1; setX++) {
+                if (outBounds(setY + node.y, setX + node.x)) continue;
+                if (matrix[node.y + setY][node.x + setX] == 1 && !array[node.y + setY][node.x + setX]) {
+                    array[node.y + setY][node.x + setX] = true;
+                    list.add(new Node(node.y + setY, node.x + setX));
+                    existFound(list.get(list.size() - 1), list);
+                }
+            }
+        }
+    }
+
+    public static boolean outBounds(int y, int x) {             // координаты вне массива
         return x < 0 || y < 0 || x >= matrix[0].length || y >= matrix.length;
     }
 
-    public static int square(byte[][] matrix) {
+    public static int square(byte[][] matrix) {            // находим площадь прямоугольника
         return (matrix.length) * matrix[0].length;
     }
 
-    public static double coefficient(int fertile, int square) {
+    public static double coefficient(int fertile, int square) {            // находим коэффициент плодородности прямоугольника
         return (double) fertile / square;
     }
 
-    public static int fertile(byte[][] matrix) {
+    public static int fertile(byte[][] matrix) {                             // количество плодородных участков на прямоугольнике
         int count = 0;
         for (byte[] bytes : matrix) {
             for (byte abytes : bytes) {
@@ -44,194 +59,69 @@ public class Main {
         return count;
     }
 
-    public static void listMatrix(byte[][] matrix) {
-
-
-        byte[][] cached = new byte[matrix.length][matrix[0].length];
-        for (int y = 0; y < cached.length; y++) {
-            System.arraycopy(matrix[y], 0, cached[y], 0, cached[y].length);
-        }
-        // List<Integer> yTemp = new ArrayList<>();
-        // List<Integer> xTemp = new ArrayList<>();
-
-        //List<Node> node = new ArrayList<>();
-        //Node startNode = null;
-        // Node endNode = null;
-        int startX = -1;
-        int endX = -1;
-        int startY = -1;
-        int endY = -1;
+    public static void listMatrix(byte[][] matrix) {                            // нахождение и занесение в список прямоугольников на данном участке
+        byte[][] result;
+        boolean flag = false;
+        List<Node> nodeList = new ArrayList<>();
         for (int y = 0; y < matrix.length; y++) {
             for (int x = 0; x < matrix[y].length; x++) {
-                if (matrix[y][x] == 2) continue;
-                if (matrix[y][x] == 1) {
-                    //                    if (cached[y][x] == 1) {
-                    //                      continue;
-                    //                }
-                    // cached[y][x] = 1;
-                    //    if (exist1(y, x, matrix) > 1) {
-                    cached[y][x] = 2;
-                    if (startX == -1) {
-                        startX = x;
-                        endX = x;
-                    } else if (startX > x) {
-                        startX = x;
-                    } else if (endX < x) {
-                        endX = x;
-                    }
-                    if (startY == -1) {
-                        startY = y;
-                        endY = y;
-                    } else if (startY > y) {
-                        startY = y;
-                    } else if (endY < y) {
-                        endY = y;
-                    }
-                } else {
-                    if (!outBounds(y, x + 1, matrix) && (!outBounds(y + 1, x, matrix))) {
-                        if (matrix[y][x] == 0 && matrix[y][x + 1] == 1 && matrix[y + 1][x] == 1) {
-                            // if (startNode == null) startNode = new Node(y, x);
-                            // else endNode = new Node(y, x);
-                            if (startX == -1) {
-                                startX = x;
-                                endX = x;
-                            } else if (startX > x) {
-                                startX = x;
-                            } else if (endX < x) {
-                                endX = x;
-                            }
-                            if (startY == -1) {
-                                startY = y;
-                                endY = y;
-                            } else if (startY > y) {
-                                startY = y;
-                            } else if (endY < y) {
-                                endY = y;
-                            }
-                            cached[y][x] = 2;
-                        } else {
-                            cached[y][x] = 2;
-                            break;
-                        }
-                    } else if (y == matrix.length - 1 && !outBounds(y, x + 1, matrix) && matrix[y][x] == 0) {
-                        cached[y][x] = 2;
-                        break;
-                    }
+                if (matrix[y][x] == 0) {
+                    array[y][x] = true;
+                    continue;
                 }
-                //            if (!outBounds(y, x + 1, matrix)) {
-                //               if (matrix[y][x + 1] ==0) {
-                //                  break;
-                //               }
-                //           }
+                if (array[y][x]) {
+                    continue;
+                }
+                if (matrix[y][x] == 1 && !array[y][x] && existNotFound(y, x)) {
+                    nodeList.add(new Node(y, x));
+                    array[y][x] = true;
+                    flag = true;
+                    break;
+                } else {
+                    array[y][x] = true;
+                }
+            }
+            if (flag) {
+                break;
             }
         }
-
-
-        // Collections.sort(xTemp);
-        // Collections.sort(yTemp);
-        int sizeY;
-        int sizeX;
-        // if (node.size() > 0) {
-        //     for (Node node1 : node) {
-        //         if (sizeY < node1.y)
-        //             sizeY = node1.y;
-        //         if (sizeX < node1.x)
-        //             sizeX = node1.x;
-        //     }
-        if ((endX - startX) + 1 > 0 && ((endY - startY) + 1 > 0)
-        && startY != -1 && endY != -1 && startX != -1 && endX != -1) {
-            sizeY = (endY - startY) + 1;
-            sizeX = (endX - startX) + 1;
-
-            byte[][] result = new byte[sizeY][sizeX];
+        if (nodeList.size() > 0) {
+            existFound(nodeList.get(0), nodeList);
+            int minY = -1;
+            int maxY = -1;
+            int minX = -1;
+            int maxX = -1;
+            for (Node node : nodeList) {
+                if (minY == -1 || minY > node.y) minY = node.y;
+                if (minX == -1 || minX > node.x) minX = node.x;
+                if (maxY == -1 || maxY < node.y) maxY = node.y;
+                if (maxX == -1 || maxX < node.x) maxX = node.x;
+            }
+            result = new byte[maxY - minY + 1][maxX - minX + 1];
             for (int y = 0; y < result.length; y++) {
-                for (int x = 0; x < result[y].length; x++) {
-                    result[y][x] = matr[startY + y][startX + x];
-                }
+                System.arraycopy(matrix[minY + y], minX, result[y], 0, result[y].length);
             }
             list.add(result);
-            matrix = cached;
-            if (fertile(matrix) > 1) {
-                listMatrix(matrix);
+        }
+        int count = 0;
+        for (boolean[] booleans : array) {
+            for (boolean aBoolean : booleans) {
+                if (!aBoolean) {
+                    count++;
+                }
             }
+        }
+        if (count > 1) {
+            listMatrix(matrix);
         }
     }
 
-    /* public static void listMatrix2(byte[][] matrix) {
-         byte[][] cached = new byte[matrix.length][matrix[0].length];
-         for (int y = 0; y < cached.length; y++) {
-             System.arraycopy(matrix[y], 0, cached[y], 0, cached[y].length);
-         }
-         // List<Integer> yTemp = new ArrayList<>();
-         // List<Integer> xTemp = new ArrayList<>();
-
-         List<Node> node = new ArrayList<>();
-         for (int y = 0; y < matrix.length; y++) {
-             for (int x = 0; x < matrix[y].length; x++) {
-                 if (matrix[y][x] == 2) continue;
-                 if (matrix[y][x] == 1) {
-                     //                    if (cached[y][x] == 1) {
-                     //                      continue;
-                     //                }
-                     // cached[y][x] = 1;
-                     //    if (exist1(y, x, matrix) > 1) {
-                     node.add(new Node(y, x));
-                     cached[y][x] = 2;
-                 } else {
-                     if (!outBounds(y, x + 1, matrix) && (!outBounds(y + 1, x, matrix))) {
-                         if (matrix[y][x] == 0 && matrix[y][x + 1] == 1 && matrix[y + 1][x] == 1) {
-                             node.add(new Node(y, x));
-                             cached[y][x] = 2;
-                         } else {
-                             cached[y][x] = 2;
-                             break;
-                         }
-                     } else if (y == matrix.length - 1 && !outBounds(y, x + 1, matrix) && matrix[y][x] == 0) {
-                         cached[y][x] = 2;
-                         break;
-                     }
-                 }
-                 //            if (!outBounds(y, x + 1, matrix)) {
-                 //               if (matrix[y][x + 1] ==0) {
-                 //                  break;
-                 //               }
-                 //           }
-             }
-         }
-
-         // Collections.sort(xTemp);
-         // Collections.sort(yTemp);
-         int sizeY = 0;
-         int sizeX = 0;
-         if (node.size() > 0) {
-             for (Node node1 : node) {
-                 if (sizeY < node1.y)
-                     sizeY = node1.y;
-                 if (sizeX < node1.x)
-                     sizeX = node1.x;
-             }
-
-             byte[][] result = new byte[sizeY + 1][sizeX + 1];
-             for (int i = 0; i < result.length; i++) {
-                 System.arraycopy(matrix[i], 0, result[i], 0, result[i].length);
-             }
-             list.add(result);
-             matrix = cached;
-             if (fertile(matrix) > 1) {
-                 listMatrix(matrix);
-             }
-         }
-     }
- */
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
-
         int n = Integer.parseInt(str.split(" ")[0]);
         int m = Integer.parseInt(str.split(" ")[1]);
-
-        byte[][] matrix = new byte[m][n];
+        matrix = new byte[m][n];
         String tempString;
         for (int y = 0; y < matrix.length; y++) {
             tempString = in.nextLine();
@@ -239,22 +129,15 @@ public class Main {
                 matrix[y][x] = Byte.parseByte(tempString.split(" ")[x]);
             }
         }
-        matr = new byte[m][n];
-        for (int y = 0; y < matrix.length; y++) {
-            System.arraycopy(matrix[y], 0, matr[y], 0, matrix[y].length);
-        }
-        //   byte[][] matrix2 = new byte[matrix.length][matrix[0].length];
-        //  for (int y = 0; y < matrix2.length; y++) {
-        //     System.arraycopy(matrix[y], 0, matrix2[y], 0, matrix2[y].length);
-        // }
-//        byte[][] cached = new byte[matrix.length][matrix[0].length];
-//        for (byte[] bytes : cached) {
-//            Arrays.fill(bytes, (byte) 0);
-//        }
+        in.close();
+        array = new boolean[m][n];
         listMatrix(matrix);
-        //  listMatrix2(matrix2);
+        if (list.isEmpty()) {
+            System.out.println(0);
+            return;
+        }
         byte[][] result = list.get(0);
-        double k = 0;
+        double k = 0.0;
         int s = 0;
         for (byte[][] bytes : list) {
             if ((k < coefficient(fertile(bytes), square(bytes))) && (square(bytes) > 1)) {
@@ -269,7 +152,6 @@ public class Main {
             }
         }
         System.out.println(square(result));
-        in.close();
     }
 
     static class Node {
@@ -282,5 +164,3 @@ public class Main {
         }
     }
 }
-
-
